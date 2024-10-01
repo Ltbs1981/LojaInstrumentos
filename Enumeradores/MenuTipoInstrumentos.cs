b13337a14d52fac97d2e7a72fc1804e366be5128
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-01 09:47:53 -0300

    criado o enumerador tipo de intrumentos

---
+++ Enumeradores/MenuTipoInstrumentos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaInstrumentos.Enumeradores
+{
+  public  enum MenuTipoInstrumentos
+    {
+        InstrumentoSemGrupo = 1,
+        Guitarra = 2,
+        TecladoOuPiano = 3,
+    }
+}
